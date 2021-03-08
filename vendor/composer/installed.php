dfd7e2358fd3d10d4da737fd3e566093e4ecb3e5
EvilDTX91 <denver912@gmail.com>
2021-03-08 15:26:33 +0100

    Composer - Twig - Bootstrap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,67 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9fee7e89119c785a064625d4f989a290e355b7e6',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9fee7e89119c785a064625d4f989a290e355b7e6',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.0.0-beta2',
+      'version' => '5.0.0.0-beta2',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e50c11b8c6434b6d68ea5897771e4d35fe12f5c3',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f3b7e2c06cc05d42936a8ad508ff1db7975cdc5',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.0.0-beta2',
+      ),
+    ),
+  ),
+);

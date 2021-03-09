2cad9fee8fcc7fd40ea7c7f83ace53da2bfa2be6
EvilDTX91 <denver912@gmail.com>
2021-03-09 19:44:27 +0100

    API strongerRobot Fix V2

--- classes/robots/api.getStrongerRobotData.php
+++ classes/robots/api.getStrongerRobotData.php
@@ -6,7 +6,8 @@ use robotwar\Settings\Connection;
 
 //API híváshoz
 //http://robotwar/classes/Robots/api.getStrongerRobotData.php?robot1=11&robot2=12
-if((isset($_GET['robot1']) && isset($_GET['robot2'])) || isset($_POST['robot1']) && isset($_POST['robot2'])){
+if((isset($_GET['robot1']) && is_numeric($_GET['robot1']) && isset($_GET['robot2']) && is_numeric($_GET['robot2']))
+        || (isset($_POST['robot1']) && is_numeric($_POST['robot1']) && isset($_POST['robot2']) && is_numeric($_POST['robot2']))){
     if($_GET['robot1'] > 0 && $_GET['robot2'] > 0){
         $robot1 = $_GET['robot1'];
         $robot2 = $_GET['robot2'];
@@ -29,4 +30,10 @@ if((isset($_GET['robot1']) && isset($_GET['robot2'])) || isset($_POST['robot1'])
         }
         echo json_encode($res);         
     }   
+}else{         
+    $res = array(
+            'response' => 0,
+            'message' => 'Hiba! Az adatlekérés sikertelen!'
+        );
+        echo json_encode($res);     
 }
